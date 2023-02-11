af61fb986652353fc4b1bb80b10d4c3a6d685121
Yash Erkn(Yasar ERKAN) <64986577+Ysrerk@users.noreply.github.com>
2023-02-10 19:53:25 -0500

    Added posting advertisement interfaces activities

---
+++ app/src/main/java/com/example/carapp/AdvertisementActivity3.java
@@ -0,0 +1,14 @@
+package com.example.carapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdvertisementActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertisement3);
+    }
+}
